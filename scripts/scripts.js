89c7f63039be6e7003096669d99eefcc06394626
Andrew Leach <51836067+AndrewLeach94@users.noreply.github.com>
2021-05-10 22:18:20 -0700

    finished random name logic

---
+++ scripts/scripts.js
@@ -0,0 +1,35 @@
+import { adjectives, nouns, verbs } from './words.js';
+
+const generateAdjectiveNumber = (max, min) => {
+    const number =  Math.floor(Math.random() * (max - min) + min);
+    return number;
+}
+
+const generateVerbNumber = (max, min) => {
+    const number =  Math.floor(Math.random() * (max - min) + min);
+    return number;
+}
+
+const generateNounNumber = (max, min) => {
+    const number =  Math.floor(Math.random() * (max - min) + min);
+    return number;
+}
+
+const triggerRandomName = () => {
+    const verbContainer = document.querySelector("#random-verb");
+    const adjectiveContainer = document.querySelector("#random-adjective");
+    const nounContainer = document.querySelector("#random-noun");
+
+    verbContainer.textContent = verbs[generateVerbNumber(633, 0)];
+    adjectiveContainer.textContent = adjectives[generateAdjectiveNumber(1099, 0)];
+    nounContainer.textContent = nouns[generateNounNumber(1937, 0)];
+}
+
+const createButtonListener = () => {
+    const trigger = document.querySelector("#button_new-name");
+    trigger.addEventListener("click", () => {
+        triggerRandomName();
+    })
+};
+
+createButtonListener();
